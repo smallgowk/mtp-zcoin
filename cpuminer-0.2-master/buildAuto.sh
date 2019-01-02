2fda59c9555673b76ac119b034370d5ef6bb2f1b
Phan Nang Duy <phanduy.itbka@gmail.com>
2019-01-03 00:47:17 +0700

    no message

---
+++ cpuminer-0.2-master/buildAuto.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo chmod +x autogen.sh
+sudo ./autogen.sh
+
+sudo chmod +x configure
+sudo ./configure --with-crypto --with-curl CFLAGS="-O2 $extracflags -DUSE_ASM -pg"
+
+sudo make -j 4
+
+sudo strip -s cpuminer
+
+sudo ./cpuminer -a mtp -o  stratum+tcp://zcoin.mintpond.com:3000 -u a3i4r4MyHMcwMWup5aMKbP2UR8jZAYBBjx.001 -p mHp4HT -p 0
